1f4df59adf1544e7b88615f5941187a9d40f08a6
Fabian Salas <fabian.salas.astete@gmail.com>
2023-11-16 16:31:14 -0300

    feat: design footer and icons

---
+++ src/components/icons/index.js
@@ -0,0 +1,9 @@
+// import { BsYoutube } from 'react-icons/bs';
+import { FaFacebookF,FaInstagram } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa";
+
+export const iconList = {
+    FaFacebookF,
+    FaInstagram ,
+    FaYoutube ,
+};
