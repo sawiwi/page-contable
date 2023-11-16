f9eee5c4b0d4ebf0731a40b626f87d10d9f01285
Fabian Salas <fabian.salas.astete@gmail.com>
2023-11-16 11:37:00 -0300

    feat:add colors tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary:{
+          default:'#0000',
+          opacity:"#0000d5",
+        },
+        secondary:{
+          default:'#ffff',
+          opacity:'#ffffd5'
+        },
+        success:{ 
+          default:"#1111",
+          opacity:'#1111d5'},
+        info:{
+          default:"#4444",
+          opacity:'#4444d5'}
+      }
+    },
   },
   plugins: [],
 }
