0389fcba8b182d5fb3dabd62d24ebcbf9b68563a
Fabian Salas <fabian.salas.astete@gmail.com>
2023-11-21 19:42:02 -0300

    feat: new section services, cards, styles, etc

---
+++ src/data/cardServices.js
@@ -0,0 +1,46 @@
+
+
+export const DataCardServices = [
+    {
+        id:1,
+        title:'Contabilidades',
+        description:'Desde que inicias tu emprendimiento, completa o simplificada.',
+        href:'',      
+    },
+    {
+        id:2,
+        title:'Procesos operacionales',
+        description:'Renta anual 14D8, pyme transparente, Renta presunta, soc de profesionales, etc',
+        href:'',      
+    },
+    {
+        id:3,
+        title:'Remuneraciones',
+        description:'a',
+        href:'',      
+    },
+    {
+        id:4,
+        title:'Servicios legales.',
+        description:'Abogados',
+        href:'',      
+    },
+    {
+        id:5,
+        title:'Administración',
+        description:'Empresas en general',
+        href:'',      
+    },
+    {
+        id:6,
+        title:'Servicio de digitación y facturación',
+        description:'a',
+        href:'',      
+    },
+    {
+        id:6,
+        title:'Conciliaciones bancarias y pericias contables',
+        description:'Enconómicos',
+        href:'',      
+    }
+]
