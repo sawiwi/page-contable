8926c116dee791960acd8725a6576fc5226e492a
Nicolas Cabrera <nicolascabreramillacan@gmail.com>
2023-11-22 11:53:04 -0300

    feat: cambio de la imagen del hero

--- src/Pages/Hero/index.js
+++ src/Pages/Hero/index.js
@@ -1,4 +1,10 @@
 import React from 'react'
+import ImageTGR from '../../assets/img/Logo de TGR.webp'
+import ImageSII from '../../assets/img/Logo de SII.webp'
+import ImageCMF from '../../assets/img/Logo de CMF.webp'
+import ImageDT from '../../assets/img/Logo de DT.webp'
+import ImagePR from '../../assets/img/Logo de Previred.webp'
+import Balanza from '../../assets/img/ley.png'
 
 const Hero = () => {
   const color = 'bg-[#070707]';
@@ -7,35 +13,98 @@ const Hero = () => {
       <div id='HeroSectionScroll' className={`min-h-[616px] ${color} pt-28 xl:pt-40`}>
         <div className={`px-4 py-8 max-w-[1276px] mx-auto ${color}`}>
           <div className="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-y-10 items-stretch">
-            <div className="flex flex-col rounded-md shadow-sm text-white">
+            <div className="flex flex-col rounded-md shadow-sm text-white justify-center">
               <p className="mx-1 max-w-xl sm:text-3xl/relaxed font-semibold">
                 Servicios de contabilidad
               </p>
-              <h1 className="bg-gradient-to-r from-[#EABE3F] via-[#732802] to-[#431600] bg-clip-text text-3xl font-bold text-transparent sm:text-5xl pb-5">
+              <h1 className="bg-gradient-to-r from-[#EABE3F] via-[#c05b28] to-[#a73e0a] bg-clip-text text-3xl font-bold text-transparent sm:text-5xl pb-5">
                 Déjanos la contabilidad a nosotros y concéntrate en tu negocio
               </h1>
 
-              <p className="mx-1 max-w-xl sm:text-xl/relaxed">
+              {/* <p className="mx-1 max-w-xl sm:text-xl/relaxed">
                 Remuneraciones, Asesoría Contable y Tributaria para Emprendedores, PyMes y Empresas. Haz que tu negocio crezca de forma ordenada y controlada.
-              </p>
+              </p> */}
 
               <div className="mt-8 flex flex-wrap justify-center gap-4">
                 <a
-                  className="block w-full rounded border border-[#EABE3F] px-12 py-3 text-sm font-medium text-white hover:bg-[#EABE3F] focus:outline-none focus:ring active:bg-[#55D6C2] sm:w-auto transition-all duration-500"
+                  className="block w-full rounded border border-[#EABE3F] px-12 py-3 text-sm font-medium text-white hover:bg-[#EABE3F] focus:outline-none focus:ring ring-[#EABE3F] active:bg-[#EABE3F] sm:w-auto transition-all duration-500"
                   href="#nosotros"
                 >
                   Nosotros
                 </a>
                 <a
-                  className="block w-full rounded border border-[#732802] px-12 py-3 text-sm font-medium text-white hover:bg-[#732802] focus:outline-none focus:ring active:bg-[#55D6C2] sm:w-auto transition-all duration-500"
+                  className="block w-full rounded border border-[#a73e0a] px-12 py-3 text-sm font-medium text-white hover:bg-[#a73e0a] focus:outline-none focus:ring ring-[#a73e0a] active:bg-[#a73e0a] sm:w-auto transition-all duration-500"
                   href="#services"
                 >
                   Nuestros Servicios
                 </a>
               </div>
             </div>
-            <div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-col-reverse">
-              <img src="https://talentocontable.cl/wp-content/uploads/2023/03/servicios-contables.png" alt="" className="" />
+            {/* <div className="grid grid-cols-3 grid-rows-3">
+              <div className='bg-white col-start-2 rounded-md my-3 mx-4 flex justify-center items-center overflow-hidden'>
+                <img src={Image1} alt="" className="" />
+              </div>
+              <div className='bg-white col-start-1 rounded-md my-3 mx-4 flex justify-center items-center overflow-hidden'>
+                <img src={Image2} alt="" className="" />
+              </div>
+              <div className='bg-white rounded-md my-3 mx-4 flex justify-center items-center overflow-hidden'>
+                <img src={Image3} alt="" className="" />
+              </div>
+              <div className='bg-white rounded-md my-3 mx-4 flex justify-center items-center overflow-hidden'>
+                <img src={Image4} alt="" className="" />
+              </div>
+
+              <div className='bg-white col-start-3 rounded-md my-3 mx-4 flex justify-center items-center overflow-hidden'>
+                <img src={Image5} alt="" className="" />
+              </div>
+
+            </div> */}
+            <div className='flex justify-center'>
+
+              <div className="relative h-[500px] w-[500px] flex items-center justify-center">
+                <div className=" relative p-10 border-2 border-dashed rounded-full border-spacing-4 border-gray-400/50">
+                  <button className="profile_item left-[20px] -top-[4px] absolute rounded-full bg-cover cursor-pointer border border-gray-400/50 p-[2px] active:scale-95 hover:scale-95 transition-all duration-500">
+                    <span className="overflow-hidden block w-[70px] h-[70px] transition-all duration-500 rounded-full z-[2] bg-white p-1">
+                    <img src={ImagePR} alt="" className="" />
+                    </span>
+                  </button>
+
+                  <button className="profile_item right-[20px] -top-[4px] absolute rounded-full bg-cover cursor-pointer border border-gray-400/50 p-[2px] active:scale-95 hover:scale-95 transition-all duration-500">
+                    <span className="overflow-hidden block w-[70px] h-[70px] transition-all duration-500 rounded-full z-[2] bg-white p-1">
+                    <img src={ImageCMF} alt="" className="" />
+                    </span>
+                  </button>
+
+
+
+                  <button className="profile_item bottom-28 -left-10 absolute rounded-full bg-cover cursor-pointer border border-gray-400/50 p-[2px] active:scale-95 hover:scale-95 transition-all duration-500">
+                    <span className="overflow-hidden block w-[70px] h-[70px] transition-all duration-500 rounded-full z-[2] bg-white p-1">
+                    <img src={ImageTGR} alt="" className="" />
+                    </span>
+                  </button>
+
+                  <button className="profile_item bottom-28 -right-10 absolute rounded-full bg-cover cursor-pointer border border-gray-400/50 p-[2px] active:scale-95 hover:scale-95 transition-all duration-500">
+                    <span className="overflow-hidden block w-[70px] h-[70px] transition-all duration-500 rounded-full z-[2] bg-white p-1">
+                    <img src={ImageDT} alt="" className="" />
+                    </span>
+                  </button>
+
+                  <button className="profile_item right-[40%] -bottom-12 absolute rounded-full bg-cover cursor-pointer border border-gray-400/50 p-[2px] active:scale-95 hover:scale-95 transition-all duration-500">
+                    <span className="overflow-hidden block w-[70px] h-[70px] transition-all duration-500 rounded-full z-[2] bg-white p-1">
+                    <img src={ImageSII} alt="" className="" />
+                    </span>
+                  </button>
+
+                  <button className="profile_item w-[280px] h-[280px] p-1 border-2 rounded-full hover:border-gray-400/50 cursor-pointer transition-all duration-500 z-0">
+                    <div className="w-full bg-white h-full flex items-center justify-center p-2 rounded-full active:scale-95 hover:scale-95 object-cover transition-all duration-500">
+                      <span className="w-40 h-40 inline-block">
+                        <img src={Balanza} alt="" className="drop-shadow-lg" />
+                      </span>
+                    </div>
+                  </button>
+                </div>
+              </div>
+
             </div>
           </div>
         </div>
