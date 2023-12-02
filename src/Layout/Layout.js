b9db7bd8c3e60551f999e8fee51d8742d5180a43
Fabian Salas <fabian.salas.astete@gmail.com>
2023-12-02 01:17:24 -0300

    feat: add title and heading

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -2,9 +2,14 @@ import React from 'react'
 import Header from '../shared/header/header'
 import Footer from '../shared/Footer'
 import ContactWsp from '../shared/ContactButton/Fab'
+import HeadPage from '../shared/HeadPage/headPage'
+
 const Layout = ({children}) => {
   return (
     <>
+        <HeadPage>
+        <title> CYVM | Consultores financieros </title>
+        </HeadPage>
         <Header/>
         <main>{children}</main>
         <Footer/>
---
+++ src/shared/HeadPage/headPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
+const HeadPage = ({children}) => {
+    return(
+        <HelmetProvider>
+            <Helmet>
+                {children}
+                <meta name="description" content="Página contable" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="keywords" content="contador, contabilidad, outsourcing, contable, empresa, crecimiento, clientes" />
+                <link rel="icon" href="/favicon.ico" />
+            </Helmet>
+        </HelmetProvider>
+    )
+}
+
+export default HeadPage
